c4426015eb8027dd26d2c1ade490b4ec0f1815ff
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-06-15 20:01:17 +0800

    update

--- icct-lms/src/components/student/QuizPage.jsx
+++ icct-lms/src/components/student/QuizPage.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { ContextVariable } from '../../context/context-config';
 import { useParams } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import StudentNavbarComponent from './StudentNavbarComponent';
 
 const QuizPage = () => {
@@ -16,9 +16,64 @@ const QuizPage = () => {
 	return (
 		<>
 			<StudentNavbarComponent />
-			<Container>
-				<p>Quiz</p>
-			</Container>
+			<div
+				className="px-4 d-flex justify-content-center align-items-center"
+				style={{ height: '70vh' }}
+			>
+				<Container
+					className=" border rounded my-5 "
+					style={{ maxWidth: '520px' }}
+				>
+					<div className="p-3 ">
+						<h4>Question</h4>
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault1"
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault1">
+								Default radio
+							</label>
+						</div>
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault2"
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault2">
+								Default checked radio
+							</label>
+						</div>
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault3"
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault3">
+								Default checked radio
+							</label>
+						</div>
+						<div className="form-check">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="flexRadioDefault4"
+							/>
+							<label className="form-check-label" htmlFor="flexRadioDefault4">
+								Default checked radio
+							</label>
+						</div>
+					</div>
+					<Button className="w-100 mb-3">Submit</Button>
+				</Container>
+			</div>
 		</>
 	);
 };
